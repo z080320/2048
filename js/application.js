9e4c689df918889b5e50db7345e555911c30ec6e
zhangys <zhangys@garena.com>
2015-05-28 17:39:57 +0800

    Change the implmentation for LeftCombine: squeeze first, then sum, then squeeze, then fill in with 0s.

--- js/application.js
+++ js/application.js
@@ -56,53 +56,150 @@ tzfeApp.controller('mainController', ['$scope', '$window', function($scope, $win
         return matrix[0].map(function (_, c) { return matrix.map(function (r) { return r[c]; }); });
     }
 
-    function leftCombineColumns(arr){
-        $window.console.log('Before Combine: ' + arr);
-        for(var j=0; j < arr.length; j++){
-            if(!arr[j]){
-                continue;
-            }
-            for(var k=j+1; k<arr.length+1; k++){
-                var original_j = j;
-                if(k < arr.length && arr[j]==arr[k]){
-                    arr[j] = 2 * arr[j];
-                    arr[k] = 0;
-                    j=k;
-                }
+    // function OBSOLETE_leftCombineColumns(arr){
+    //     $window.console.log('Before Combine: ' + arr);
+    //     for(var j=0; j < arr.length; j++){
+    //         if(!arr[j]){
+    //             continue;
+    //         }
+    //         for(var k=j+1; k<arr.length+1; k++){
+    //             var original_j = j;
+    //             if(k < arr.length && arr[j]==arr[k]){
+    //                 arr[j] = 2 * arr[j];
+    //                 arr[k] = 0;
+    //                 j=k;
+    //             }
 
-                var previousNonZeroPos = original_j -1;
-                while(previousNonZeroPos >= 0){
-                    if(previousNonZeroPos>=0 && arr[previousNonZeroPos]){
-                        break;
-                    }
-                    previousNonZeroPos--;
-                }
+    //             var previousNonZeroPos = original_j -1;
+    //             while(previousNonZeroPos >= 0){
+    //                 if(previousNonZeroPos>=0 && arr[previousNonZeroPos]){
+    //                     break;
+    //                 }
+    //                 previousNonZeroPos--;
+    //             }
 
-                $window.console.debug('previousNonZeroPos: '+ previousNonZeroPos);
-                var firstZeroPos = previousNonZeroPos +1;                
-                if(firstZeroPos < original_j){
-                    arr[firstZeroPos] = arr[original_j];
-                    arr[original_j] = 0;
-                }
+    //             $window.console.debug('previousNonZeroPos: '+ previousNonZeroPos);
+    //             var firstZeroPos = previousNonZeroPos +1;                
+    //             if(firstZeroPos < original_j){
+    //                 arr[firstZeroPos] = arr[original_j];
+    //                 arr[original_j] = 0;
+    //             }
+    //         }
+    //     }
+
+    //     $window.console.log('After combine: '+arr);
+    //     return arr;
+    // }
+
+
+    function leftSumArray(arr){
+        $window.console.debug('Before Sum: '+ arr);
+        for(var i=0; i< arr.length; i++){
+            if(arr[i+1]==arr[i]){
+                arr[i] = arr[i] * 2;
+                arr[i+1]=0;
+                i++;
             }
         }
+        $window.console.debug('After Sum: '+ arr);
+        return arr;
+    }
 
-        $window.console.log('After combine: '+arr);
+
+
+    function testLeftSumArray(){
+        var arr;
+        arr = [2, 4, 8];
+        leftSumArray(arr);
+        arr = [2, 4, 4];
+        leftSumArray(arr);
+        arr = [2, 2];
+        leftSumArray(arr);
+        arr = [2, 2,  2];
+        leftSumArray(arr);
+        arr = [2, 2, 2, 2];
+        leftSumArray(arr);
+        arr = [2];
+        leftSumArray(arr);
+        arr = [8, 8, 16, 16];
+        leftSumArray(arr);
+        arr = [4, 4, 32, 64];
+        leftSumArray(arr);
+    }
+
+    // testLeftSumArray();
+
+    function leftSqueezeArray(arr){
+        $window.console.debug('Before Squeeze: '+ arr);
+        var squeezed_arr = [];
+        for(var i=0; i<arr.length; i++){
+            if(arr[i]){
+                squeezed_arr.push(arr[i]);
+            }
+        }
+        $window.console.debug('After Squeeze: '+ squeezed_arr);
+        return squeezed_arr;
+    }
+
+    function testLeftSqueezeArray(){
+        var arr;
+        arr = [2, 0, 0, 0];
+        leftSqueezeArray(arr);
+        arr = [0, 2, 4, 4];
+        leftSqueezeArray(arr);
+        arr = [2, 0, 0, 2];
+        leftSqueezeArray(arr);
+        arr = [2, 2, 0, 2];
+        leftSqueezeArray(arr);
+        arr = [2, 2, 2, 2];
+        leftSqueezeArray(arr);
+        arr = [0, 0, 0, 2];
+        leftSqueezeArray(arr);
+        arr = [8, 8, 16, 0];
+        leftSqueezeArray(arr);
+        arr = [4, 4, 32, 0];
+        leftSqueezeArray(arr);
+    }
+
+    // testLeftSqueezeArray();
+
+    // Sum the neighbouring same numbers
+    // move all numbers to the left
+    function leftCombineColumns(arr){
+        $window.console.log('Before Combine: '+ arr);
+
+        var arr_length = arr.length,
+            squeezed_arr = leftSqueezeArray(arr),
+            summed_arr = leftSumArray(squeezed_arr),
+            arr = leftSqueezeArray(summed_arr), 
+            length_diff = arr_length - arr.length;
+        $window.console.log('Length Difference: '+ length_diff);
+
+        if(length_diff){
+            arr = arr.concat(Array.apply(null, new Array(length_diff)).map(Number.prototype.valueOf, 0));
+        }
+
+        $window.console.log('After Combine: '+ arr);
         return arr;
     }
 
     function testLeftCombineColumns(){
-        var arr = [2, 0, 0, 0];
+        var arr;
+        arr = [2, 0, 0, 0];
         leftCombineColumns(arr);
-        var arr = [0, 2, 4, 4];
+        arr = [0, 2, 4, 4];
         leftCombineColumns(arr);
-        var arr = [2, 0, 0, 2];
+        arr = [2, 0, 0, 2];
         leftCombineColumns(arr);
-        var arr = [2, 2, 0, 2];
+        arr = [2, 2, 0, 2];
         leftCombineColumns(arr);
-        var arr = [2, 2, 2, 2];
+        arr = [2, 2, 2, 2];
         leftCombineColumns(arr);
-        var arr = [0, 0, 0, 2];
+        arr = [0, 0, 0, 2];
+        leftCombineColumns(arr);
+        arr = [8, 8, 16, 0];
+        leftCombineColumns(arr);
+        arr = [4, 4, 32, 0];
         leftCombineColumns(arr);
     }
 
